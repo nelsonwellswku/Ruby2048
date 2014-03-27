f3980342a91d84e4638fb49452933af30e430612
nelsonwellswku <nelsonwellswku@gmail.com>
2014-03-26 20:58:08 -0500

    More up tests, de-dup 0 array code

--- t/ArrayValueSliderTests.rb
+++ t/ArrayValueSliderTests.rb
@@ -2,22 +2,22 @@ require "minitest/autorun"
 require_relative "../lib/ArrayValueSlider"
 
 class ArrayValueSliderTests < Minitest::Test
-  
+
   def test_simple_slide_up
 
     # arrange
     arr = [
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
+      zeros,
+      zeros,
+      zeros,
       [0, 2, 0, 2]
     ]
  
     expected_arr = [
       [0, 2, 0, 2],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0]
+      zeros,
+      zeros,
+      zeros
     ]
 
     # act
@@ -29,20 +29,123 @@ class ArrayValueSliderTests < Minitest::Test
     
   end
 
+  def test_simple_impact_no_merge_up
+    
+    # arrange
+    arr = [
+      [0, 0, 4, 0],
+      [4, 2, 0, 0],
+      [0, 4, 2, 2],
+      [2, 0, 0, 4]
+    ]
+
+    expected_arr = [
+      [4, 2, 4, 2],
+      [2, 4, 2, 4],
+      zeros,
+      zeros 
+    ]
+
+    # act
+    slider = ArrayValueSlider.new
+    slider.up!(arr)
+   
+    # assert
+    assert_equal(expected_arr, arr)
+  end
+  
+  def test_simple_impact_merge_up
+    
+    # arrange
+    arr = [
+      zeros,
+      [2, 4, 8, 16],
+      zeros,
+      [2, 4, 8, 16]
+    ]
+
+    expected_arr = [
+      [4, 8, 16, 32],
+      zeros,
+      zeros,
+      zeros
+    ]
+
+    # act
+    slider = ArrayValueSlider.new
+    slider.up!(arr)
+
+    # assert
+    assert_equal(expected_arr, arr)
+
+  end
+
+  def test_three_impact_merge_up
+
+    # arrange
+    arr = [
+      zeros,
+      [2, 4, 8, 16],
+      [2, 4, 8, 16],
+      [2, 4, 8, 16]
+    ]   
+    
+    expected_arr = [
+      [4, 8, 16, 32],
+      [2, 4, 8, 16],
+      zeros,
+      zeros
+    ]
+
+    # act
+    slider = ArrayValueSlider.new
+    slider.up!(arr)    
+    
+    # assert
+    assert_equal(expected_arr, arr)
+
+  end
+
+  def test_four_impact_merge_up
+
+    # arrange
+    arr = [
+      [2, 4, 8, 16],
+      [2, 4, 8, 16],
+      [2, 0, 8, 16],
+      [2, 4, 8, 16]
+    ]   
+    
+    expected_arr = [
+      [4, 8, 16, 32],
+      [4, 4, 16, 32],
+      zeros,
+      zeros
+    ]
+
+    # act
+    slider = ArrayValueSlider.new
+    slider.up!(arr)    
+    
+    # assert
+    assert_equal(expected_arr, arr)
+
+  end
+
   def test_simple_slide_down
     
     # arrange
     arr = [
       [2, 0, 2, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0]
+      zeros,
+      zeros,
+      zeros
     ]
 
     expected_arr = [
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
+      zeros,
+      zeros,
+      zeros,
       [2, 0, 2, 0]
     ]
 
@@ -59,16 +162,16 @@ class ArrayValueSliderTests < Minitest::Test
     
     # arrange
     arr = [
-      [0, 0, 0, 0],
+      zeros,
       [2, 0, 0, 0],
-      [0, 0, 0, 0],
+      zeros,
       [2, 0, 0, 0]
     ] 
 
     expected_arr = [
-      [0, 0, 0, 0],
+      zeros,
       [0, 0, 0, 2],
-      [0, 0, 0, 0],
+      zeros,
       [0, 0, 0, 2]
     ]
     
@@ -85,16 +188,16 @@ class ArrayValueSliderTests < Minitest::Test
     # arrange
     arr = [
       [0, 0, 0, 2],
-      [0, 0, 0, 0],
+      zeros,
       [0, 0, 0, 2],
-      [0, 0, 0, 0]
+      zeros
     ] 
 
     expected_arr = [
       [2, 0, 0, 0],
-      [0, 0, 0, 0],
+      zeros,
       [2, 0, 0, 0],
-      [0, 0, 0, 0]
+      zeros
     ]
     
     # act
@@ -105,4 +208,10 @@ class ArrayValueSliderTests < Minitest::Test
     assert_equal(expected_arr, arr)
   end
 
+  private
+
+  def zeros 
+    [0, 0, 0, 0]
+  end
+  
 end
